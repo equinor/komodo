48d481f1173e113c4e9472256eee90a39f5cbbcf
Pål Grønås Drange <PGDR@statoil.com>
2017-12-13 17:59:38 +0100

    Lint dependencies

--- lint.py
+++ lint.py
@@ -5,15 +5,22 @@ import yaml as yml
 
 from collections import namedtuple
 
-kerr = namedtuple('KomodoError', ['pkg', 'version', 'maintainer', 'err'])
+kerr = namedtuple('KomodoError',
+                  ['pkg', 'version', 'maintainer', 'depends', 'err'])
 
 MISSING_PACKAGE = 'missing package'
 MISSING_VERSION = 'missing version'
+MISSING_DEPENDENCY = 'missing dependency'
 MISSING_MAINTAINER = 'missing maintainer'
 
 
-def _kerr(pkg=None, version=None, maintainer=None, err=None):
-    return kerr(pkg=pkg, version=version, maintainer=maintainer, err=err)
+def _kerr(pkg=None, version=None, maintainer=None, depends=None, err=None):
+    return kerr(
+        pkg=pkg,
+        version=version,
+        maintainer=maintainer,
+        depends=depends,
+        err=err)
 
 
 def _validate(pkg, ver, repo):
@@ -26,11 +33,39 @@ def _validate(pkg, ver, repo):
     return _kerr(pkg=pkg, version=ver, maintainer=repo[pkg][ver]['maintainer'])
 
 
+def lint_maintainers(pkgs, repo):
+    return [_validate(pkg, ver, repo) for pkg, ver in pkgs.items()]
+
+
+def lint_dependencies(pkgs, repo):
+    errs = []
+    for pkg, ver in pkgs.items():
+        pv = repo[pkg][ver]
+        maintainer = pv['maintainer']
+        if 'depends' not in pv:
+            continue
+        missing = [d for d in pv['depends'] if d not in pkgs]
+        if missing:
+            errs.append(
+                _kerr(
+                    pkg=pkg,
+                    version=ver,
+                    maintainer=maintainer,
+                    depends=missing,
+                    err=MISSING_DEPENDENCY))
+    return errs
+
+
 def lint(pkgfile, repofile):
     with open(pkgfile) as p, open(repofile) as r:
         pkgs, repo = yml.load(p), yml.load(r)
 
-    return [_validate(pkg, ver, repo) for pkg, ver in pkgs.items()]
+    mns = lint_maintainers(pkgs, repo)
+    if any([err.err for err in mns]):
+        return mns, []
+
+    deps = lint_dependencies(pkgs, repo)
+    return mns, deps
 
 
 if __name__ == '__main__':
@@ -39,15 +74,15 @@ if __name__ == '__main__':
     parser.add_argument('pkgfile', type=str)
     parser.add_argument('repofile', type=str)
     args = parser.parse_args()
-    errs = lint(args.pkgfile, args.repofile)
-    print('%d packages' % len(errs))
-    has_error = any([err.err for err in errs])
-    if not has_error:
+    mns, deps = lint(args.pkgfile, args.repofile)
+    print('%d packages' % len(mns))
+    if not any([err.err for err in mns + deps]):
         print('No errors found')
         exit(0)
 
-    for err in errs:
+    for err in mns + deps:
         if err.err:
             ver = err.version if err.version else ''
-            print('%s for %s %s' % (err.err, err.pkg, ver))
+            dep = ': %s' % ', '.join(err.depends) if err.depends else ''
+            print('%s for %s %s%s' % (err.err, err.pkg, ver, dep))
     exit('Error in komodo configuration.')
