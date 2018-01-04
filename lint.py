3ddc839fb5c3e1ab4757f6e16ded400cacb0e12b
Pål Grønås Drange <PGDR@statoil.com>
2018-01-04 10:29:18 +0100

    Add version number linting

--- lint.py
+++ lint.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 from __future__ import print_function
-
-import yaml as yml
+import argparse
+import distutils.version
+from pkg_resources import parse_version
 import logging
+import yaml as yml
 
 from collections import namedtuple
 
@@ -13,6 +15,8 @@ MISSING_PACKAGE = 'missing package'
 MISSING_VERSION = 'missing version'
 MISSING_DEPENDENCY = 'missing dependency'
 MISSING_MAINTAINER = 'missing maintainer'
+MALFORMED_VERSION = 'malformed version'
+FLOAT_VERSION = 'dangerous version (float interpretable)'
 
 
 def _kerr(pkg=None, version=None, maintainer=None, depends=None, err=None):
@@ -38,13 +42,40 @@ def lint_maintainers(pkgs, repo):
     return [_validate(pkg, ver, repo) for pkg, ver in pkgs.items()]
 
 
+def lint_version_numbers(pkgs, repo):
+    errs = []
+    for pkg, ver in pkgs.items():
+        pv = repo[pkg][ver]
+        maintainer = pv.get('maintainer', MISSING_MAINTAINER)
+        if isinstance(ver, float):
+            errs.append(
+                _kerr(
+                    pkg=pkg,
+                    version=ver,
+                    maintainer=maintainer,
+                    err=FLOAT_VERSION))
+        try:
+            v = parse_version(ver)
+            logging.info('Using %s %s' % (pkg, v))
+            if ver.startswith('v') and len(ver) > 1 and '0' <= ver[1] <= '9':
+                v = distutils.version.StrictVersion(ver[1:])
+            else:
+                v = distutils.version.StrictVersion(ver)
+        except:
+            errs.append(
+                _kerr(
+                    pkg=pkg,
+                    version=ver,
+                    maintainer=maintainer,
+                    err=MALFORMED_VERSION))
+    return errs
+
+
 def lint_dependencies(pkgs, repo):
     errs = []
     for pkg, ver in pkgs.items():
-        if isinstance(ver, float):
-            logging.warn('dangerous version for %s (float interpretable)' % pkg)
         pv = repo[pkg][ver]
-        maintainer = pv['maintainer']
+        maintainer = pv.get('maintainer', MISSING_MAINTAINER)
         if 'depends' not in pv:
             continue
         missing = [d for d in pv['depends'] if d not in pkgs]
@@ -64,26 +95,36 @@ def lint(pkgfile, repofile):
         pkgs, repo = yml.load(p), yml.load(r)
 
     mns = lint_maintainers(pkgs, repo)
-    if any([err.err for err in mns]):
-        return mns, []
-
     deps = lint_dependencies(pkgs, repo)
-    return mns, deps
+    vers = lint_version_numbers(pkgs, repo)
+    return mns, deps, vers
 
 
-if __name__ == '__main__':
-    import argparse
+def get_args():
     parser = argparse.ArgumentParser(description='lint komodo setup')
     parser.add_argument('pkgfile', type=str)
     parser.add_argument('repofile', type=str)
+    parser.add_argument(
+        '--verbose',
+        help="Massive amount of outputs",
+        action="store_const",
+        dest="loglevel",
+        const=logging.INFO)
     args = parser.parse_args()
-    mns, deps = lint(args.pkgfile, args.repofile)
+    return args
+
+
+if __name__ == '__main__':
+    args = get_args()
+    logging.basicConfig(format='%(message)s', level=args.loglevel)
+
+    mns, deps, vers = lint(args.pkgfile, args.repofile)
     print('%d packages' % len(mns))
-    if not any([err.err for err in mns + deps]):
+    if not any([err.err for err in mns + deps + vers]):
         print('No errors found')
         exit(0)
 
-    for err in mns + deps:
+    for err in mns + deps + vers:
         if err.err:
             ver = err.version if err.version else ''
             dep = ': %s' % ', '.join(err.depends) if err.depends else ''
