b808a1b7b9036b9f3c9476264ae970a0fd23b448
Pål Grønås Drange <PGDR@statoil.com>
2018-02-26 20:17:48 +0100

    Add cleanup: finding unused versions

--- komodo/__init__.py
+++ komodo/__init__.py
@@ -4,6 +4,7 @@ from .build import make, pypaths
 from .fetch import fetch
 from .shell import shell, pushd
 from .lint import lint
+from .cleanup import cleanup
 from .maintainer import maintainers
 
 __version__ = '1.0'
---
+++ komodo/cleanup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import logging
+import yaml as yml
+
+def cleanup(repofile, releasefiles):
+    with open(repofile, 'r') as r:
+        repo = yml.load(r)
+    rels = []
+    for fname in releasefiles:
+        with open(fname, 'r') as f:
+            rels.append(yml.load(f))
+
+    if not isinstance(repo, dict):
+        raise ValueError('Malformed package file: %s ' % str(type(repo)))
+    for rel in rels:
+        if not isinstance(rel, dict):
+            raise ValueError('Malformed repository file: %s' % str(type(rel)))
+
+    registered_versions = []
+    for pkg in repo:
+        for v in repo[pkg]:
+            registered_versions.append((pkg,v))
+
+    seen_versions = set()
+    for rel in rels:
+        for pkg in rel:
+            seen_versions.add((pkg, rel[pkg]))
+
+    seen_all = True
+    for ver in registered_versions:
+        if ver not in seen_versions:
+            if seen_all:
+                print('unused:')
+                seen_all = False
+            print('  - %s: %s' % ver)
+    if seen_all:
+        print('ok')
+
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 3:
+        exit('usage: komodo.cleanup repo.yml rel1.yml rel2.yml ... reln.yml')
+
+    repo = sys.argv[1]
+    releases = sys.argv[2:]
+    cleanup(repo, releases)
