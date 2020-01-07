11073df6fd039338dd12da5e04c176bad059f02f
Asmund Birkeland <musiv@equinor.com>
2020-01-07 16:15:10 +0100

    black the symlink module

--- komodo/symlink/create_links.py
+++ komodo/symlink/create_links.py
@@ -1,4 +1,4 @@
-import os,sys
+import os, sys
 import argparse
 import json
 from .sanity_check import verify_integrity
@@ -15,47 +15,48 @@ def working_dir(path):
         os.chdir(prev_dir)
 
 
-def _create_link(src, dst, link_dict):  
+def _create_link(src, dst, link_dict):
     if src in link_dict and not os.path.exists(src):
         _create_link(link_dict[src], src, link_dict)
 
     if not os.path.exists(src):
         raise ValueError("{} does not exist".format(src))
-    
+
     if os.path.exists(dst) and os.path.islink(dst):
         if os.readlink(dst) == src:
             return
         os.remove(dst)
-    
+
     os.symlink(src, dst)
 
 
 def create_symlinks(links_dict):
-    root_folder = links_dict['root_folder']
+    root_folder = links_dict["root_folder"]
     if not os.path.isabs(root_folder):
         raise ValueError("The root folder specified is not absolute")
 
     if not os.path.isdir(root_folder):
         raise ValueError("The folder {} does not exist".format(root_folder))
 
-    with working_dir(root_folder):  
-        for dst, src in links_dict['links'].items():
-            _create_link(src, dst, links_dict['links'])
+    with working_dir(root_folder):
+        for dst, src in links_dict["links"].items():
+            _create_link(src, dst, links_dict["links"])
 
 
 def symlink_main():
 
-    parser = argparse.ArgumentParser(description='Create symlinks for komodo versions.')
-    parser.add_argument('config', type=str,
-                        help='a json file describing symlink structure')
+    parser = argparse.ArgumentParser(description="Create symlinks for komodo versions.")
+    parser.add_argument(
+        "config", type=str, help="a json file describing symlink structure"
+    )
 
     args = parser.parse_args()
     if not os.path.isfile(args.config):
-        sys.exit('the file {} can,not be found'.format(args.config))
-    
+        sys.exit("the file {} can,not be found".format(args.config))
+
     with open(args.config) as input_file:
         input_dict = json.load(input_file)
-    
+
     errors = verify_integrity(input_dict)
     if errors:
         print("The following errors where found in the config file:")
--- tests/test_link_io_structure.py
+++ tests/test_link_io_structure.py
@@ -1,9 +1,9 @@
 from komodo.symlink.sanity_check import (
-    read_link_structure, 
-    equal_links, 
-    assert_root_nodes, 
-    verify_integrity, 
-    sanity_main
+    read_link_structure,
+    equal_links,
+    assert_root_nodes,
+    verify_integrity,
+    sanity_main,
 )
 from komodo.symlink.create_links import create_symlinks, symlink_main
 
@@ -20,29 +20,25 @@ def _get_test_root():
 
 def test_read_folder_structure(tmpdir):
     with tmpdir.as_cwd():
-        os.mkdir('2012.01.rc1')
-        os.mkdir('2012.01.rc2')
-        os.mkdir('bleeding')
-        os.mkdir('2012.01.12')
+        os.mkdir("2012.01.rc1")
+        os.mkdir("2012.01.rc2")
+        os.mkdir("bleeding")
+        os.mkdir("2012.01.12")
 
-        os.symlink('2012.01.12', '2012.01')
-        os.symlink('2012.01', 'stable')
-        os.symlink('2012.01', 'testing')
-        os.symlink('2012.01.rc2', 'unstable')
+        os.symlink("2012.01.12", "2012.01")
+        os.symlink("2012.01", "stable")
+        os.symlink("2012.01", "testing")
+        os.symlink("2012.01.rc2", "unstable")
 
         expected_result = {
-            'root_folder': tmpdir,
-            'root_links' : [
-                'stable',
-                'unstable',
-                'testing'
-            ],
-            'links': {
-                '2012.01': '2012.01.12',
-                'stable': '2012.01',
-                'testing': '2012.01',
-                'unstable': '2012.01.rc2'
-            }
+            "root_folder": tmpdir,
+            "root_links": ["stable", "unstable", "testing"],
+            "links": {
+                "2012.01": "2012.01.12",
+                "stable": "2012.01",
+                "testing": "2012.01",
+                "unstable": "2012.01.rc2",
+            },
         }
 
         output_dict = read_link_structure(os.getcwd())
@@ -53,195 +49,179 @@ def test_read_folder_structure(tmpdir):
 
 def test_create_symlinks(tmpdir):
     links_dict = {
-        'root_folder': tmpdir,
-        'links': {
-            '2012.01': '2012.01.12',
-            'stable': '2012.01',
-            'testing': '2012.01',
-            'unstable': '2012.01.rc2'
-        }
+        "root_folder": tmpdir,
+        "links": {
+            "2012.01": "2012.01.12",
+            "stable": "2012.01",
+            "testing": "2012.01",
+            "unstable": "2012.01.rc2",
+        },
     }
 
     with tmpdir.as_cwd():
-        os.mkdir('2012.01.12')
-        os.mkdir('2012.01.rc2')
+        os.mkdir("2012.01.12")
+        os.mkdir("2012.01.rc2")
         create_symlinks(links_dict)
 
-        assert os.path.exists('stable')
-        assert os.path.islink('stable')
-        assert os.readlink('stable') == '2012.01'
-        assert os.path.realpath('stable') == os.path.realpath('2012.01.12')
-        assert os.path.realpath('unstable') == os.path.realpath('2012.01.rc2')
+        assert os.path.exists("stable")
+        assert os.path.islink("stable")
+        assert os.readlink("stable") == "2012.01"
+        assert os.path.realpath("stable") == os.path.realpath("2012.01.12")
+        assert os.path.realpath("unstable") == os.path.realpath("2012.01.rc2")
 
 
 def test_integration(tmpdir):
-    
-    test_folder = _get_test_root()
-    shutil.copy(os.path.join(test_folder, 'data/links.json'), tmpdir)
-    with tmpdir.as_cwd():
-        with open('links.json') as link_file:
-            input_dict = json.load(link_file)
-        
-        input_dict['root_folder'] = os.path.realpath(input_dict['root_folder'])
 
-        os.mkdir(input_dict['root_folder'])
-        os.mkdir(os.path.join(input_dict['root_folder'], '2012.01.12'))
-        os.mkdir(os.path.join(input_dict['root_folder'], '2012.01.rc1'))
+    test_folder = _get_test_root()
+    shutil.copy(os.path.join(test_folder, "data/links.json"), tmpdir)
+    with tmpdir.as_cwd():
+        with open("links.json") as link_file:
+            input_dict = json.load(link_file)
+
+        input_dict["root_folder"] = os.path.realpath(input_dict["root_folder"])
+
+        os.mkdir(input_dict["root_folder"])
+        os.mkdir(os.path.join(input_dict["root_folder"], "2012.01.12"))
+        os.mkdir(os.path.join(input_dict["root_folder"], "2012.01.rc1"))
 
         create_symlinks(input_dict)
 
-        output = read_link_structure(input_dict['root_folder'])
+        output = read_link_structure(input_dict["root_folder"])
         assert equal_links(input_dict, output)
 
 
 def test_root_links(tmpdir):
-    
+
     test_folder = _get_test_root()
-    shutil.copy(os.path.join(test_folder, 'data/links.json'), tmpdir)
+    shutil.copy(os.path.join(test_folder, "data/links.json"), tmpdir)
     with tmpdir.as_cwd():
-        with open('links.json') as link_file:
+        with open("links.json") as link_file:
             input_dict = json.load(link_file)
-        
+
         assert_root_nodes(input_dict)
 
-        input_dict['root_links'].append('non_exisiting')
+        input_dict["root_links"].append("non_exisiting")
 
         with pytest.raises(AssertionError):
             assert_root_nodes(input_dict)
 
 
 def test_link_integrity(tmpdir):
-     with tmpdir.as_cwd():
-        os.mkdir('2012.01.rc2')
-        os.mkdir('2012.01.12')
+    with tmpdir.as_cwd():
+        os.mkdir("2012.01.rc2")
+        os.mkdir("2012.01.12")
 
         test_dict = {
-            'root_folder': tmpdir,
-            'root_links' : [
-                'stable',
-                'unstable',
-                'testing'
-            ],
-            'links': {
-                '2012.01': '2012.01.12',
-                'stable': '2012.01',
-                'testing': '2012.01',
-                'unstable': '2012.01.rc2'
-            }
+            "root_folder": tmpdir,
+            "root_links": ["stable", "unstable", "testing"],
+            "links": {
+                "2012.01": "2012.01.12",
+                "stable": "2012.01",
+                "testing": "2012.01",
+                "unstable": "2012.01.rc2",
+            },
         }
-        
+
         errors = verify_integrity(test_dict)
         assert errors == []
 
-        test_dict['links']['bleeding'] = 'bleeding_py3'
+        test_dict["links"]["bleeding"] = "bleeding_py3"
         errors = verify_integrity(test_dict)
-        assert errors == ['bleeding_py3 does not exist']
+        assert errors == ["bleeding_py3 does not exist"]
 
-        test_dict['links']['2012.01.rc2'] = 'unstable'
+        test_dict["links"]["2012.01.rc2"] = "unstable"
         errors = verify_integrity(test_dict)
-        assert '2012.01.rc2 is part of a cyclic symlink' in errors
-        assert 'unstable is part of a cyclic symlink' in errors
+        assert "2012.01.rc2 is part of a cyclic symlink" in errors
+        assert "unstable is part of a cyclic symlink" in errors
 
 
 def test_root_folder_error(tmpdir):
-     with tmpdir.as_cwd():
-        os.mkdir('2012.01.12')
+    with tmpdir.as_cwd():
+        os.mkdir("2012.01.12")
 
         test_dict = {
-            'root_folder': os.path.join(str(tmpdir), 'non_existing'),
-            'root_links' : [
-                'stable',
-                'unstable',
-                'testing'
-            ],
-            'links': {
-                '2012.01': '2012.01.12',
-                'stable': '2012.01',
-                'testing': '2012.01',
-                'unstable': '2012.01.rc2'
-            }
+            "root_folder": os.path.join(str(tmpdir), "non_existing"),
+            "root_links": ["stable", "unstable", "testing"],
+            "links": {
+                "2012.01": "2012.01.12",
+                "stable": "2012.01",
+                "testing": "2012.01",
+                "unstable": "2012.01.rc2",
+            },
         }
-        
+
         with pytest.raises(ValueError) as e:
             create_symlinks(test_dict)
-        assert 'does not exist' in str(e.value)
-        
-        test_dict['root_folder'] = os.path.relpath(str(tmpdir))
+        assert "does not exist" in str(e.value)
+
+        test_dict["root_folder"] = os.path.relpath(str(tmpdir))
         with pytest.raises(ValueError) as e:
             create_symlinks(test_dict)
-        assert 'is not absolute' in str(e.value)
+        assert "is not absolute" in str(e.value)
 
 
 def test_link_error(tmpdir):
-     with tmpdir.as_cwd():
-        os.mkdir('2012.01.12')
+    with tmpdir.as_cwd():
+        os.mkdir("2012.01.12")
 
         test_dict = {
-            'root_folder': tmpdir,
-            'root_links' : [
-                'stable',
-                'unstable',
-                'testing'
-            ],
-            'links': {
-                '2012.01': '2012.01.12',
-                'stable': '2012.01',
-                'testing': '2012.01',
-                'unstable': '2012.01.rc2'
-            }
+            "root_folder": tmpdir,
+            "root_links": ["stable", "unstable", "testing"],
+            "links": {
+                "2012.01": "2012.01.12",
+                "stable": "2012.01",
+                "testing": "2012.01",
+                "unstable": "2012.01.rc2",
+            },
         }
         with pytest.raises(ValueError) as e:
             create_symlinks(test_dict)
-        assert '2012.01.rc2 does not exist' == str(e.value)
+        assert "2012.01.rc2 does not exist" == str(e.value)
 
 
 def test_root_link_error(tmpdir):
-     with tmpdir.as_cwd():
-        os.mkdir('2012.01.12')
+    with tmpdir.as_cwd():
+        os.mkdir("2012.01.12")
 
         test_dict = {
-            'root_folder': tmpdir,
-            'root_links' : [
-                'stable',
-                'unstable',
-                'testing'
-            ],
-            'links': {
-                '2012.01': '2012.01.12',
-                'stable': '2012.01',
-                'unstable': '2012.01.rc2'
-            }
+            "root_folder": tmpdir,
+            "root_links": ["stable", "unstable", "testing"],
+            "links": {
+                "2012.01": "2012.01.12",
+                "stable": "2012.01",
+                "unstable": "2012.01.rc2",
+            },
         }
         with pytest.raises(ValueError) as e:
             create_symlinks(test_dict)
-        assert '2012.01.rc2 does not exist' == str(e.value)
+        assert "2012.01.rc2 does not exist" == str(e.value)
 
 
 def test_executables(tmpdir):
     old_argv = sys.argv.copy()
-    sys.argv = ['run', 'links_test.json']
+    sys.argv = ["run", "links_test.json"]
     try:
         test_folder = _get_test_root()
-        with open(os.path.join(test_folder,'data/links_full.json')) as input_file:
+        with open(os.path.join(test_folder, "data/links_full.json")) as input_file:
             input_data = json.load(input_file)
-            
+
         with tmpdir.as_cwd():
-            input_data['root_folder'] = str(tmpdir)
-            with open('links_test.json', 'w') as test_file:
+            input_data["root_folder"] = str(tmpdir)
+            with open("links_test.json", "w") as test_file:
                 test_file.write(json.dumps(input_data))
 
             with pytest.raises(SystemExit):
                 symlink_main()
-            
-            os.mkdir('bleeding-py36')
-            os.mkdir('bleeding-py27')
-            os.mkdir('2020.01.a0-py36')
-            os.mkdir('2020.01.a0-py27')
-            os.mkdir('2019.12.00-py36')
-            os.mkdir('2019.12.00-py27')
-            
+
+            os.mkdir("bleeding-py36")
+            os.mkdir("bleeding-py27")
+            os.mkdir("2020.01.a0-py36")
+            os.mkdir("2020.01.a0-py27")
+            os.mkdir("2019.12.00-py36")
+            os.mkdir("2019.12.00-py27")
+
             symlink_main()
-            
+
             sanity_main()
     finally:
         sys.argv = old_argv
