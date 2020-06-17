cddb98f545a6dca667c2b4d4737e8ba00a833277
Åsmund Birkeland <musiv@equinor.com>
2020-06-17 13:42:57 +0200

    Add load and write yaml using prettify

--- komodo/prettier.py
+++ komodo/prettier.py
@@ -12,52 +12,122 @@ import ruamel.yaml
 
 
 def repository_specific_formatting(empty_line_top_level, yaml_string):
-    '''Transform function to ruamel.yaml's dump function. Makes sure there are
+    """Transform function to ruamel.yaml's dump function. Makes sure there are
     only empty lines inbetween different top level keys (if empty_line_top_level
     is True, otherwise no empty lines).
-    '''
+    """
 
-    yaml_string = re.sub(r'\n+', r'\n', yaml_string)  # Remove all empty lines
+    yaml_string = re.sub(r"\n+", r"\n", yaml_string)  # Remove all empty lines
 
     if empty_line_top_level:
         yaml_string = re.sub(  # Add one empty line between each package
-            r'(\n[^\#][^\n]*)\n([^\s])', r'\1\n\n\2', yaml_string
+            r"(\n[^\#][^\n]*)\n([^\s])", r"\1\n\n\2", yaml_string
         )
 
     return yaml_string
 
 
 def is_repository(config):
-    '''Returns `False` if the configuration corresponds to a Komodo release
+    """Returns `False` if the configuration corresponds to a Komodo release
     (all config elements below top level key are strings). Returns `True` if
     it corresponds to a _repository_ (all config elements below top level key are
     themselves dictionaries).
 
     Raises ValueError if inconsistent throughout the config.
-    '''
+    """
 
     # For Python2+3 compatibility. On Python3-only, use isinstance(x, str)
     # instead of isinstance(x, basestring).
     try:
         basestring
     except NameError:
-        basestring = str # No basestring on Python3
+        basestring = str  # No basestring on Python3
 
     if all([isinstance(package, basestring) for package in config.values()]):
         return False
-    elif all([isinstance(package, ruamel.yaml.comments.CommentedMap)
-              for package in config.values()]):
+    elif all(
+        [
+            isinstance(package, ruamel.yaml.comments.CommentedMap)
+            for package in config.values()
+        ]
+    ):
         return True
 
     raise ValueError(
-        'Inconsistent configuration file. '
-        'Not able to detect if it is a release or repository.'
+        "Inconsistent configuration file. "
+        "Not able to detect if it is a release or repository."
     )
 
 
-def prettier(yaml_input_string):
-    '''Takes in a string corresponding to a YAML Komodo configuration, and returns
-    the corresponding prettified YAML string.'''
+def prettier(yaml_input_dict):
+    """Takes in a string corresponding to a YAML Komodo configuration, and returns
+    the corresponding prettified YAML string."""
+
+    ruamel_instance = ruamel.yaml.YAML()
+    ruamel_instance.indent(  # Komodo prefers two space indendation
+        mapping=2, sequence=4, offset=2
+    )
+    ruamel_instance.width = 1000  # Avoid ruamel wrapping long
+
+    komodo_repository = is_repository(yaml_input_dict)
+
+    # On Python3.6+, sorted_config can just be an
+    # ordinary dict as insertion order is then preserved.
+    sorted_config = ruamel.yaml.comments.CommentedMap()
+    for package in sorted(yaml_input_dict, key=str.lower):
+        sorted_config[package] = yaml_input_dict[package]
+
+    setattr(sorted_config, ruamel.yaml.comments.comment_attrib, yaml_input_dict.ca)
+
+    yaml_output = StringIO()
+    ruamel_instance.dump(
+        sorted_config,
+        yaml_output,
+        transform=functools.partial(repository_specific_formatting, komodo_repository),
+    )
+
+    if sys.version_info < (3, 0):
+        # Need to encode the byte-string on Python2
+        return yaml_output.getvalue().encode("utf-8")
+
+    return yaml_output.getvalue()
+
+
+def prettified_yaml(filepath, check_only=True):
+    """Returns `True` if the file is already "prettified", `False` otherwise.
+    If `check_only` is False, the input file will be "prettified" in place if necessary.
+    """
+
+    print("Checking {}... ".format(filepath), end="")
+    with open(file=filepath, mode="r") as input_file:
+        yaml_original = input_file.read()
+
+    yaml_input = load_yaml(filepath)
+
+    yaml_prettified_string = prettier(yaml_input)
+
+    if yaml_prettified_string != yaml_original:
+        print("{} reformatted!".format("would be" if check_only else ""))
+        if not check_only:
+            with open(filepath, "w") as fh:
+                fh.write(yaml_prettified_string)
+        return False
+
+    print("looking good!")
+    return True
+
+
+def write_to_file(repository, filename):
+    if type(repository) == dict:
+        repository = ruamel.yaml.comments.CommentedMap(repository)
+    output_str = prettier(repository)
+    with open(file=filename, mode="w") as output_file:
+        output_file.write(output_str)
+
+
+def load_yaml(filename):
+    if not os.path.isfile(os.path.realpath(filename)):
+        raise argparse.ArgumentTypeError("{} is not a valid file".format(filename))
 
     ruamel_instance = ruamel.yaml.YAML()
     ruamel_instance.indent(  # Komodo prefers two space indendation
@@ -66,92 +136,14 @@ def prettier(yaml_input_string):
     ruamel_instance.width = 1000  # Avoid ruamel wrapping long
 
     try:
-        config = ruamel_instance.load(yaml_input_string)
-    except ruamel.yaml.constructor.DuplicateKeyError as e:
-        raise SystemExit(str(e))
+        with open(filename) as repo_handle:
+            input_dict = ruamel_instance.load(repo_handle)
+        return input_dict
 
-    komodo_repository = is_repository(config)
-
-    # On Python3.6+, sorted_config can just be an
-    # ordinary dict as insertion order is then preserved.
-    sorted_config = ruamel.yaml.comments.CommentedMap()
-    for package in sorted(config, key=str.lower):
-        sorted_config[package] = config[package]
-
-    setattr(sorted_config, ruamel.yaml.comments.comment_attrib, config.ca)
-
-    yaml_output = StringIO()
-    ruamel_instance.dump(
-        sorted_config,
-        yaml_output,
-        transform=functools.partial(repository_specific_formatting, komodo_repository)
-    )
-
-    if sys.version_info < (3, 0):
-        # Need to encode the byte-string on Python2
-        return yaml_output.getvalue().encode('utf-8')
-
-    return yaml_output.getvalue()
-
-
-def prettified_yaml(filepath, check_only=True):
-    '''Returns `True` if the file is already "prettified", `False` otherwise.
-    If `check_only` is False, the input file will be "prettified" in place if necessary.
-    '''
-
-    print('Checking {}... '.format(filepath), end='')
-
-    with open(filepath, 'r') as fh:
-        yaml_input_string = fh.read()
-
-    yaml_prettified_string = prettier(yaml_input_string)
-
-    if yaml_prettified_string != yaml_input_string:
-        print('{} reformatted!'.format('would be' if check_only else ''))
-        if not check_only:
-            with open(filepath, 'w') as fh:
-                fh.write(yaml_prettified_string)
-        return False
-
-    print('looking good!')
-    return True
-
-
-def prettier_main():
-    '''Main function doing user argument parsing and calling necessary functions.
-    '''
-
-    parser = argparse.ArgumentParser(
-        description=(
-            'Check and/or format the Komodo configuration files. '
-            'Takes in any number of yml files, which could be e.g. the main '
-            'Komodo repository and an arbitrary number of releases. '
-            'Throws a hard error if the same package is defined multiple times.'
+    except (
+        ruamel.yaml.scanner.ScannerError,
+        ruamel.yaml.constructor.DuplicateKeyError,
+    ) as e:
+        raise SystemExit(
+            "The file: <{}> contains invalid YAML syntax:\n {}".format(filename, str(e))
         )
-    )
-    parser.add_argument(
-        'files',
-        type=lambda arg: arg if os.path.isfile(arg) \
-            else parser.error('{} is not a file'.format(arg)),
-        nargs='+',
-        help='One or more files to format/check',
-    )
-    parser.add_argument(
-        '--check',
-        action='store_true',
-        help=(
-            'Do not write the files back, just return the status. '
-            'Return code 0 means nothing would change. '
-            'Return code 1 means some files would be reformatted.'
-        ),
-    )
-
-    args = parser.parse_args()
-
-    sys.exit(0) if all(
-        [prettified_yaml(filename, args.check) for filename in args.files]
-    ) or not args.check else sys.exit(1)
-
-
-if __name__ == '__main__':
-    prettier_main()
--- tests/test_prettier.py
+++ tests/test_prettier.py
@@ -12,32 +12,39 @@ INPUT_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), "input"
 
 
 class TestPrettier(unittest.TestCase):
-
     @staticmethod
     def get_yaml_string(filename):
         with open(os.path.join(INPUT_FOLDER, filename)) as fh:
             return fh.read()
 
     def test_repository_prettifying(self):
-        ugly_repository = TestPrettier.get_yaml_string('ugly_repository.yml')
-        pretty_repository = TestPrettier.get_yaml_string('pretty_repository.yml')
-        self.assertEqual(komodo.prettier(ugly_repository), pretty_repository)
+        pretty_repository = TestPrettier.get_yaml_string("pretty_repository.yml")
+        self.assertEqual(
+            komodo.prettier(
+                komodo.load_yaml(os.path.join(INPUT_FOLDER, "ugly_repository.yml"))
+            ),
+            pretty_repository,
+        )
 
     def test_release_prettifying(self):
-        ugly_release = TestPrettier.get_yaml_string('ugly_release.yml')
-        pretty_release = TestPrettier.get_yaml_string('pretty_release.yml')
-        self.assertEqual(komodo.prettier(ugly_release), pretty_release)
+        pretty_release = TestPrettier.get_yaml_string("pretty_release.yml")
+        self.assertEqual(
+            komodo.prettier(
+                komodo.load_yaml(os.path.join(INPUT_FOLDER, "ugly_release.yml"))
+            ),
+            pretty_release,
+        )
 
     def test_duplicate_entries(self):
-        duplicate_repository = TestPrettier.get_yaml_string('duplicate_repository.yml')
         with self.assertRaises(SystemExit):
-            komodo.prettier(duplicate_repository)
+            komodo.load_yaml(os.path.join(INPUT_FOLDER, "duplicate_repository.yml"))
 
     def test_inconsistent_config(self):
-        inconsistent_config = TestPrettier.get_yaml_string('inconsistent_config.yml')
         with self.assertRaises(ValueError):
-            komodo.prettier(inconsistent_config)
+            komodo.prettier(
+                komodo.load_yaml(os.path.join(INPUT_FOLDER, "inconsistent_config.yml"))
+            )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
