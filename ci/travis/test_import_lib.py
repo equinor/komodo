1f3e42f84bc06181f1536e8bdb1f72c7139b4624
Zohar Malamant <zom@equinor.com>
2020-06-20 01:28:28 +0200

    Add shim workaround

---
+++ ci/travis/test_import_lib.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import sys
+import os
+from ctypes import CDLL, c_char_p
+
+
+lib = CDLL("libkmd.so")
+foo = lib.foo
+foo.restype = c_char_p
+foo.argtypes = ()
+
+# Test that libkmd.so is loaded correctly
+val = foo()
+if val != b"bar":
+    sys.stderr.write("foo() returned '{}' instead of 'bar'\n".format(val))
+    sys.exit(1)
