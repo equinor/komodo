f63b759ce6853064eb4602bc3bff93799831fdb3
Zohar Malamant <zom@equinor.com>
2020-07-23 12:49:09 +0200

    : Begin ci_helper.sh

---
+++ jenkins/ci_helper.sh
@@ -0,0 +1,58 @@
+function ci_install_cmake {
+    pip install cmake ninja
+
+    local root=${KOMODO_ROOT}/${CI_KOMODO_RELEASE}/root
+
+    export CMAKE_GENERATOR=Ninja
+    export CMAKE_PREFIX_PATH=$root
+
+    # Colour!
+    export CFLAGS="${CFLAGS:-} -fdiagnostics-color=always"
+    export CXXFLAGS="${CXXFLAGS:-} -fdiagnostics-color=always"
+    export LDFLAGS="${LDFLAGS:-} -fdiagnostics-color=always"
+
+    export LD_LIBRARY_PATH=$root/lib:$root/lib64
+}
+
+copy_test_files () {
+    if [ -d "$CI_SOURCE_ROOT/tests" ]; then
+        cp -r $CI_SOURCE_ROOT/tests $CI_TEST_ROOT/tests
+    fi
+    if [ -d "$CI_SOURCE_ROOT/test-data" ]; then
+        cp -r $CI_SOURCE_ROOT/test-data $CI_TEST_ROOT/test-data
+    fi
+}
+
+install_package () {
+    pip install .
+}
+
+install_test_dependencies () {
+    if [ -f "test_requirements.txt" ]; then
+        pip install -r test_requirements.txt
+    else
+        pip install pytest
+    fi
+}
+
+start_tests () {
+    pytest
+}
+
+run_tests_default () {
+    copy_test_files
+
+    if [ ! -z "${CI_PR_RUN:-}" ]
+    then
+        install_package
+    fi
+
+    install_test_dependencies
+
+    pushd $CI_TEST_ROOT
+    start_tests
+}
+
+function run_tests {
+    run_tests_default
+}
