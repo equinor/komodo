cbe13d0e6e4c5c2e95912e8488a9ee581ba3a62d
Jonas G. Drange <jond@equinor.com>
2020-09-17 09:12:18 +0200

    Catchall migration warning in activator

--- komodo/switch.py
+++ komodo/switch.py
@@ -2,6 +2,9 @@ import os
 from jinja2 import Template
 
 
+MIGRATION_WARNING = """Attention! Your machine is running on a RHEL6 environment which is in the process of being phased out.\\nNew versions of komodo will from October 2020 only support RHEL7.\\nPlease migrate as soon as possible. If you have any questions or issues - contact us on #ert-users on Slack or Equinor's Yammer!"""
+
+
 def create_activator_switch(data, prefix, release):
     """Given a prefix and a release, create an activator switch which
     will vary the selected activator based on the RHEL version. The data
@@ -24,12 +27,16 @@ def create_activator_switch(data, prefix, release):
         data.get("activator_switch.tmpl")
     ) as activator_tmpl:
         activator.write(
-            Template(activator_tmpl.read(), keep_trailing_newline=True).render(prefix=prefix, release=release_py)
+            Template(activator_tmpl.read(), keep_trailing_newline=True).render(
+                prefix=prefix, release=release_py, migration_warning=MIGRATION_WARNING
+            )
         )
 
     with open(os.path.join(release_path, "enable.csh"), "w") as activator, open(
         data.get("activator_switch.csh.tmpl")
     ) as activator_tmpl:
         activator.write(
-            Template(activator_tmpl.read(), keep_trailing_newline=True).render(prefix=prefix, release=release_py)
+            Template(activator_tmpl.read(), keep_trailing_newline=True).render(
+                prefix=prefix, release=release_py, migration_warning=MIGRATION_WARNING
+            )
         )
