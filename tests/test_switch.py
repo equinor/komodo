04f5be611a083b6b2d37a5a1306e136cb2b0d337
larsevj <levje@equinor.com>
2024-01-23 09:59:24 +0100

    Support for rhel8 and python311 in activator switch

--- tests/test_switch.py
+++ tests/test_switch.py
@@ -7,8 +7,8 @@ from komodo.switch import MIGRATION_WARNING
 
 def test_write_activator_switches(tmpdir):
     prefix = tmpdir / "prefix"
-    release = "2020.01.01-py27-rhel6"
-    expected_release = "2020.01.01-py27"
+    release = "2022.01.01-py38-rhel7"
+    expected_release = "2022.01.01-py38"
     switch.create_activator_switch(Data(), prefix, release)
 
     actual_bash_activator = prefix / f"{expected_release}/enable"
@@ -22,8 +22,17 @@ if [[ $(uname -r) == *el7* ]] ; then
     source $KOMODO_ROOT/$KOMODO_RELEASE_REAL-rhel7/enable
     export PS1="(${{KOMODO_RELEASE_REAL}}) ${{_PRE_KOMODO_PS1}}"
     export KOMODO_RELEASE=$KOMODO_RELEASE_REAL
-else
+elif [[ $(uname -r) == *el8* ]] ; then
+    export KOMODO_ROOT={prefix}
+    KOMODO_RELEASE_REAL={expected_release}
+
+    source $KOMODO_ROOT/$KOMODO_RELEASE_REAL-rhel8/enable
+    export PS1="(${{KOMODO_RELEASE_REAL}}) ${{_PRE_KOMODO_PS1}}"
+    export KOMODO_RELEASE=$KOMODO_RELEASE_REAL
+elif [[ $(uname -r) == *el6* ]]; then
     echo -e "{MIGRATION_WARNING}"
+else
+    echo "Attention! Your machine is running on an environment that is not supported."
 fi
 """.strip()
     )
@@ -41,10 +50,40 @@ if ( `uname -r` =~ *el7* ) then
         set prompt = "[$KOMODO_RELEASE_REAL] $_KOMODO_OLD_PROMPT"
     endif
     setenv KOMODO_RELEASE $KOMODO_RELEASE_REAL
-else
+elif ( `uname -r` =~ *el8* ) then
+    setenv KOMODO_ROOT {prefix}
+    set KOMODO_RELEASE_REAL = "{expected_release}"
+
+    source $KOMODO_ROOT/$KOMODO_RELEASE_REAL-rhel8/enable.csh
+    if ( $?_KOMODO_OLD_PROMPT ) then
+        set prompt = "[$KOMODO_RELEASE_REAL] $_KOMODO_OLD_PROMPT"
+    endif
+    setenv KOMODO_RELEASE $KOMODO_RELEASE_REAL
+elif ( `uname -r` =~ *el6* ) then
     echo -e "{MIGRATION_WARNING}"
+else
+    echo "Attention! Your machine is running on an environment that is not supported."
 endif
 """.strip()
+    )
+
+
+def test_write_activator_switches_for_py311(tmpdir):
+    prefix = tmpdir / "prefix"
+    release = "2024.01.01-py311-rhel8"
+    expected_release = "2024.01.01-py311"
+    switch.create_activator_switch(Data(), prefix, release)
+
+    actual_bash_activator = prefix / f"{expected_release}/enable"
+    assert (
+        "Error! Komodo release for Python newer than 3.8 is not available on RHEL7."
+        in actual_bash_activator.read_text(encoding="utf-8").strip()
+    )
+
+    actual_csh_activator = prefix / f"{expected_release}/enable.csh"
+    assert (
+        "Error! Komodo release for Python newer than 3.8 is not available on RHEL7."
+        in actual_csh_activator.read_text(encoding="utf-8").strip()
     )
 
 
