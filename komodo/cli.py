c7b77edf29ae71d02569fce7623b3484aa65c43b
Jonathan Karlsen <JONAK@equinor.com>
2024-02-07 10:37:26 +0100

    Refactor komodo cli
    Refactored komodo cli into smaller functions. This should make reading and
    testing the cli easier.

--- komodo/cli.py
+++ komodo/cli.py
@@ -6,12 +6,12 @@ import sys
 import uuid
 import warnings
 from pathlib import Path
-from typing import List, Tuple
+from typing import Any, Callable, Dict, List, Mapping, Optional, Sequence, Tuple, Union
 
 import jinja2
 from ruamel.yaml import YAML
 
-from komodo import switch
+import komodo.switch
 from komodo.build import make
 from komodo.data import Data
 from komodo.fetch import fetch
@@ -23,6 +23,47 @@ from komodo.package_version import (
 from komodo.shebang import fixup_python_shebangs
 from komodo.shell import pushd, shell
 from komodo.yaml_file_types import ReleaseFile, RepositoryFile
+
+
+class KomodoNamespace(argparse.Namespace):
+    """
+    Komodo argument parser namespace
+    """
+
+    pkgs: ReleaseFile
+    repo: RepositoryFile
+    prefix: str
+    release: str
+    tmp: str
+    downloads: str
+    jobs: int
+    download: bool
+    build: bool
+    install: bool
+    dry_run: bool
+    overwrite: bool
+    cmake: str
+    pip: str
+    pyver: Optional[str]
+    workspace: str
+    extra_data_dirs: List[str]
+    postinst: str
+
+
+def profile_time(msg: str) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
+    def decorator(fun: Callable[..., Any]) -> Callable[..., Any]:
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            start_time = datetime.datetime.now()
+            res = fun(*args, **kwargs)
+            timings.append(
+                (msg, datetime.datetime.now() - start_time),
+            )
+            _print_timing(timings[-1])
+            return res
+
+        return wrapper
+
+    return decorator
 
 
 def create_enable_scripts(komodo_prefix: str, komodo_release: str) -> None:
@@ -52,7 +93,7 @@ def create_enable_scripts(komodo_prefix: str, komodo_release: str) -> None:
         )
 
 
-def _print_timings(
+def _print_timing(
     timing_element: Tuple[str, datetime.timedelta],
     adjust: bool = False,
 ) -> None:
@@ -62,138 +103,184 @@ def _print_timings(
         print(f" * {timing_element[0]} took {timing_element[1]}")
 
 
-def _main(args):
-    timings: List[Tuple[str, datetime.timedelta]] = []
-    abs_prefix = Path(args.prefix).resolve()
+def check_for_possible_build_overwrite(
+    release_path: Path, overwrite_enabled: bool = False
+) -> None:
+    """Checks for possible overwrite of prior build.
 
-    data = Data(extra_data_dirs=args.extra_data_dirs)
+    Args:
+        release_path: The path where the new release will be built.
+        overwrite_enabled: Flag to enable overwriting of prior release build. Defaults to False.
 
-    if args.download or (not args.build and not args.install):
-        start_time = datetime.datetime.now()
-        git_hashes = fetch(
-            args.pkgs.content,
-            args.repo.content,
-            outdir=args.downloads,
-            pip=args.pip,
-        )
-        timings.append(("Fetching all packages", datetime.datetime.now() - start_time))
-        _print_timings(timings[-1])
-
-    if args.download and not args.build:
-        sys.exit(0)
-
-    if (
-        not args.overwrite
-        and (Path(args.prefix) / args.release).exists()
-        and "bleeding" not in args.release
+    Raises:
+        RuntimeError: if prior release build is detected,
+        --overwrite flag is not set, and build is not 'bleeding'.
+    """
+    if release_path.exists() and not (
+        overwrite_enabled or "bleeding" in str(release_path)
     ):
         raise RuntimeError(
             "Only bleeding builds can be overwritten unless --overwrite is supplied"
         )
 
-    # append root to the temporary build dir, as we want a named root/
-    # directory as the distribution root, organised under the distribution name
-    tmp_prefix = abs_prefix / args.release / "root"
+
+def is_download_only(args: KomodoNamespace) -> bool:
+    return args.download and not args.build
+
+
+@profile_time("Fetching all packages")
+def download_packages(
+    release_file_content: Mapping[str, str],
+    repository_file_content: Mapping[str, Mapping[str, Union[str, Sequence[str]]]],
+    download_destination: str,
+    pip_executable: str = "pip",
+) -> Dict[str, str]:
+    """Downloads all PyPI packages to destination. Tries to download other
+        packages to destination too.
+    Git packages are collected, and a dict of all git hashes is returned.
+
+    Args:
+    ----
+    release_file_content: A mapping of all packages and versions to be
+        included in the release.
+    repository_file_content: A mapping of all available packages and
+        versions that can be included in a release.
+    download_destination: A string of the path where packages should
+        be downloaded to
+    pip_executable: A string of the pip executable to use. Defaults to 'pip'
+
+    Returns:
+    --
+    Dict of git hashes found
+    """
+    git_hashes = fetch(
+        release_file_content,
+        repository_file_content,
+        outdir=download_destination,
+        pip=pip_executable,
+    )
+
+    return git_hashes
+
+
+@profile_time("Building non-pip part of komodo in workspace")
+def _make(*args, **kwargs) -> None:
+    make(*args, **kwargs)
+
+
+def build_packages_and_move_to_release_path(
+    args: KomodoNamespace, data: Data, tmp_prefix: Path
+):
     fakeroot = Path(args.release).resolve()
-    if args.build or not args.install:
-        start_time = datetime.datetime.now()
-        make(
-            args.pkgs.content,
-            args.repo.content,
-            data,
-            prefix=str(tmp_prefix),
-            dlprefix=args.downloads,
-            builddir=args.tmp,
-            jobs=args.jobs,
-            cmk=args.cmake,
-            pip=args.pip,
-            fakeroot=str(fakeroot),
-        )
-        timings.append(
-            (
-                "Building non-pip part of komodo in workspace",
-                datetime.datetime.now() - start_time,
-            ),
-        )
-        _print_timings(timings[-1])
+    _make(
+        args.pkgs.content,
+        args.repo.content,
+        data,
+        prefix=str(tmp_prefix),
+        dlprefix=args.downloads,
+        builddir=args.tmp,
+        jobs=args.jobs,
+        cmk=args.cmake,
+        pip=args.pip,
+        fakeroot=str(fakeroot),
+    )
 
-        shell(f"mv {args.release + str(tmp_prefix)} {args.release}")
-        with contextlib.suppress(OSError):
-            os.removedirs(f"{args.release + str(tmp_prefix.parent)}")
+    shell(f"mv {args.release + str(tmp_prefix)} {args.release}")
+    with contextlib.suppress(OSError):
+        os.removedirs(f"{args.release + str(tmp_prefix.parent)}")
 
-    if args.build and not args.install:
-        sys.exit(0)
 
-    create_enable_scripts(komodo_prefix=tmp_prefix, komodo_release=args.release)
+def is_build_only(args: KomodoNamespace) -> bool:
+    return args.build and not args.install
 
-    releasedoc = Path(args.release) / Path(args.release)
-    with open(releasedoc, "w", encoding="utf-8") as filehandle:
-        release = {}
-        for pkg, ver in args.pkgs.content.items():
-            entry = args.repo.content[pkg][ver]
-            maintainer = args.repo.content[pkg][ver]["maintainer"]
-            if ver == LATEST_PACKAGE_ALIAS:
-                ver = latest_pypi_version(entry.get("pypi_package_name", pkg))
-            elif args.repo.content[pkg][ver].get("fetch") == "git":
-                ver = git_hashes[pkg]
-            release[pkg] = {
-                "version": ver,
+
+def generate_general_release_packages(release_path: Path) -> Path:
+    """Append root to the temporary build dir, as we want a named root/
+    directory as the distribution root, organised under the distribution name
+
+    Args:
+        release_path (Path):
+
+    Returns:
+        release_root
+    """
+    abs_prefix = release_path.resolve()
+    return abs_prefix / "root"
+
+
+def generate_release_packages(
+    release_name: Path,
+    release_file_content: Mapping[str, str],
+    repository_file_content: Mapping[str, Mapping[str, Union[str, Sequence[str]]]],
+    git_hashes: Optional[Mapping[str, str]] = None,
+) -> None:
+    releasedoc = Path(release_name) / Path(release_name)
+    with open(releasedoc, mode="w", encoding="utf-8") as filehandle:
+        release: Dict[str, Dict[str, str]] = {}
+        for package, version in release_file_content.items():
+            entry: Dict[str, str] = repository_file_content[package][version]
+            maintainer = repository_file_content[package][version]["maintainer"]
+            if version == LATEST_PACKAGE_ALIAS:
+                version = latest_pypi_version(entry.get("pypi_package_name", package))
+            elif entry.get("fetch") == "git":
+                version = git_hashes[package]
+            release[package] = {
+                "version": version,
                 "maintainer": maintainer,
             }
         yaml = YAML()
         yaml.dump(release, filehandle)
 
-    if args.dry_run:
-        return
 
-    print(f"Installing {args.release} to {args.prefix}")
+@profile_time("Rsyncing partial komodo to destination")
+def rsync_komodo_to_destination(release_name: str, destination: str) -> None:
+    shell(f"mv {release_name} .{release_name}")
+    shell(f"rsync -a .{release_name} {destination}")
 
-    start_time = datetime.datetime.now()
-    shell(f"mv {args.release} .{args.release}")
-    shell(f"rsync -a .{args.release} {args.prefix}")
-    timings.append(
-        (
-            "Rsyncing partial komodo to destination",
-            datetime.datetime.now() - start_time,
-        ),
-    )
-    _print_timings(timings[-1])
 
-    prefix_path = Path(args.prefix)
-    release_path = prefix_path / Path(args.release)
-
+def move_old_release_from_release_path_if_exists(release_path: Path) -> None:
     if release_path.exists():
-        shell(
-            f"mv {args.prefix}/{args.release} "
-            f"{args.prefix}/{args.release}.delete-{uuid.uuid4()}",
-        )
+        shell(f"mv {str(release_path)} " f"{str(release_path)}.delete-{uuid.uuid4()}")
 
-    shell(
-        f"mv {args.prefix}/.{args.release} {args.prefix}/{args.release}",
-    )
-    start_time = datetime.datetime.now()
+
+def move_new_release_to_release_path(args: KomodoNamespace, release_path: Path) -> None:
+    shell(f"mv {args.prefix}/.{args.release} {str(release_path)}")
+
+
+@profile_time("Deleting previous release")
+def delete_old_previously_moved_releases(prefix_path: Path, release_name: Path) -> None:
 
     release_dir_glob = [
-        str(p.absolute()) for p in list(prefix_path.glob(f"{args.release}.delete-*"))
+        str(p.absolute()) for p in list(prefix_path.glob(f"{release_name}.delete-*"))
     ]
-
     shell(
         "rm -rf -- " + " ".join(release_dir_glob),
         allow_failure=True,
     )
-    timings.append(("Deleting previous release", datetime.datetime.now() - start_time))
-    _print_timings(timings[-1])
 
-    if args.tmp:
-        # Allows e.g. pip to use this folder as a destination for "pip
-        # download", instead of in some cases falling back to /tmp, which is
-        # undesired when building on nfs.
-        os.environ["TMPDIR"] = args.tmp
 
-    release_root = release_path / "root"
-    start_time = datetime.datetime.now()
-    for pkg, ver in args.pkgs.content.items():
-        current = args.repo.content[pkg][ver]
+def apply_fallback_tmpdir_for_pip_if_set(tmp_dir: Optional[str] = None):
+    """Allows e.g. pip to use this folder as a destination for "pip
+    download", instead of in some cases falling back to /tmp, which is
+    undesired when building on nfs.
+
+    Args:
+        tmp_dir (Optional): Directory for pip to use as fallback.
+    """
+    if tmp_dir:
+        os.environ["TMPDIR"] = tmp_dir
+
+
+@profile_time("pip install to final destination")
+def install_previously_downloaded_pip_packages(
+    release_file_content: Mapping[str, str],
+    repository_file_content: Mapping[str, Mapping[str, Union[str, Sequence[str]]]],
+    downloads_directory: str,
+    pip_executable: str,
+    release_root: Path,
+) -> None:
+    for pkg, ver in release_file_content.items():
+        current = repository_file_content[pkg][ver]
         if current["make"] != "pip":
             continue
 
@@ -201,7 +288,7 @@ def _main(args):
         if ver == LATEST_PACKAGE_ALIAS:
             ver = latest_pypi_version(package_name)
         shell_input = [
-            args.pip,
+            pip_executable,
             f"install {package_name}=={strip_version(ver)}",
             "--prefix",
             str(release_root),
@@ -209,43 +296,117 @@ def _main(args):
             "--no-deps",
             "--ignore-installed",
             # assuming fetch.py has done "pip download" to this directory:
-            f"--cache-dir {args.downloads}",
-            f"--find-links {args.downloads}",
+            f"--cache-dir {downloads_directory}",
+            f"--find-links {downloads_directory}",
         ]
         shell_input.append(current.get("makeopts"))
 
         print(shell(shell_input))
-    timings.append(
-        ("pip install to final destination", datetime.datetime.now() - start_time),
-    )
-    _print_timings(timings[-1])
 
-    fixup_python_shebangs(args.prefix, args.release)
 
-    switch.create_activator_switch(data, args.prefix, args.release)
+def run_post_installation_scripts_if_set(
+    postinst: Optional[str], release_path: Path
+) -> None:
+    if postinst:
+        timing_func = profile_time("Running post-install scripts")
+        timing_func(shell([postinst, release_path]))
 
-    if args.postinst:
-        start_time = datetime.datetime.now()
-        shell([args.postinst, release_path])
-        timings.append(
-            ("Running post-install scripts", datetime.datetime.now() - start_time),
-        )
-        _print_timings(timings[-1])
 
-    start_time = datetime.datetime.now()
+@profile_time("find and delete python bytecode files")
+def find_and_delete_python_bytecode_files(release_root: Path) -> None:
     print("running", f"find {release_root} -name '*.pyc' -delete")
     shell(f"find {release_root} -name '*.pyc' -delete")
 
-    print("Setting permissions", [data.get("set_permissions.sh"), release_path])
-    shell([data.get("set_permissions.sh"), str(release_path)])
-    timings.append(
-        ("Cleanup *.pyc and fix permissions", datetime.datetime.now() - start_time),
+
+@profile_time("set permissions")
+def set_permissions(set_permissions_script: Path, release_path: Path) -> None:
+    print("Setting permissions", [set_permissions_script, release_path])
+    shell([str(set_permissions_script), str(release_path)])
+
+
+@profile_time("Cleanup *.pyc and fix permissions")
+def cleanup_python_bytecode_files_and_fix_permissions(
+    release_root: Path, set_permissions_script_path, release_path: Path
+):
+    find_and_delete_python_bytecode_files(release_root)
+    set_permissions(set_permissions_script_path, release_path)
+
+
+timings: List[Tuple[str, datetime.timedelta]] = []
+
+
+def _main(args: KomodoNamespace) -> None:
+    """Create a Komodo release.
+
+    Args:
+    ----
+        args: KomodoNamespace instance with configuration
+    """
+    data = Data(extra_data_dirs=args.extra_data_dirs)
+
+    if args.download or (not args.build and not args.install):
+        git_hashes = download_packages(
+            args.pkgs.content,
+            args.repo.content,
+            download_destination=args.downloads,
+            pip_executable=args.pip,
+        )
+        if is_download_only(args):
+            sys.exit(0)
+
+    prefix_path = Path(args.prefix)
+    release_path = prefix_path / args.release
+    check_for_possible_build_overwrite(
+        release_path=release_path, overwrite_enabled=args.overwrite
     )
-    _print_timings(timings[-1])
+
+    release_root = generate_general_release_packages(release_path)
+
+    if args.build or not args.install:
+        build_packages_and_move_to_release_path(args, data, release_root)
+        if is_build_only(args):
+            sys.exit(0)
+
+    create_enable_scripts(komodo_prefix=release_root, komodo_release=args.release)
+
+    generate_release_packages(
+        args.release, args.pkgs.content, args.repo.content, git_hashes
+    )
+
+    if args.dry_run:
+        return
+
+    print(f"Installing {args.release} to {args.prefix}")
+
+    rsync_komodo_to_destination(args.release, destination=prefix_path)
+
+    move_old_release_from_release_path_if_exists(release_path)
+    move_new_release_to_release_path(args, release_path)
+    delete_old_previously_moved_releases(prefix_path, args.release)
+
+    apply_fallback_tmpdir_for_pip_if_set(args.tmp)
+
+    install_previously_downloaded_pip_packages(
+        args.pkgs.content,
+        args.repo.content,
+        downloads_directory=args.downloads,
+        pip_executable=args.pip,
+        release_root=release_root,
+    )
+    fixup_python_shebangs(args.prefix, args.release)
+
+    komodo.switch.create_activator_switch(data, args.prefix, args.release)
+
+    run_post_installation_scripts_if_set(args.postinst, release_path)
+    cleanup_python_bytecode_files_and_fix_permissions(
+        release_root,
+        set_permissions_script_path=data.get("set_permissions.sh"),
+        release_path=release_path,
+    )
 
     print("Time report:")
     for timing_element in timings:
-        _print_timings(timing_element, adjust=True)
+        _print_timing(timing_element, adjust=True)
 
 
 def cli_main():
@@ -259,7 +420,7 @@ def cli_main():
         _main(args)
 
 
-def parse_args(args: List[str]) -> argparse.Namespace:
+def parse_args(args: List[str]) -> KomodoNamespace:
     """Parse the arguments from the command line into an `argparse.Namespace`.
     Having a separated function makes it easier to test the CLI.
 
@@ -444,7 +605,7 @@ def parse_args(args: List[str]) -> argparse.Namespace:
         ),
     )
 
-    args = parser.parse_args(args)
+    args: KomodoNamespace = parser.parse_args(args, namespace=KomodoNamespace())
 
     if args.pyver is not None:
         message = (
