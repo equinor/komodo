b7dbfb922cc5694d7024005bf3eb9f34bfe28f50
Jonathan Karlsen <JONAK@equinor.com>
2024-02-08 13:28:12 +0100

    Remove sudo

--- komodo/shell.py
+++ komodo/shell.py
@@ -15,16 +15,13 @@ def pushd(path):
     os.chdir(prev)
 
 
-def shell(cmd: str, sudo=False, allow_failure=False) -> bytes:
+def shell(cmd: str, allow_failure=False) -> bytes:
     try:
         cmdlist = cmd.split(" ")
     except AttributeError:
         # was already a list, but to make sure every arg has its own entry,
         # re-join and split
         cmdlist = " ".join(filter(None, cmd)).split(" ")
-
-    if sudo:
-        cmdlist = ["sudo", *cmdlist]
 
     prompt = f"[{os.getcwd()}]>"
     print(prompt, " ".join(cmdlist))
