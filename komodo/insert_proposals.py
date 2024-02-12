2e0e83482ca92ccbb47aaffc345915f812dd888c
Jonathan Karlsen <JONAK@equinor.com>
2024-02-12 14:34:55 +0100

    Improve insert proposals

--- komodo/insert_proposals.py
+++ komodo/insert_proposals.py
@@ -1,39 +1,62 @@
 import argparse
-import collections
 import contextlib
 import difflib
 import os
 from base64 import b64decode
 from datetime import datetime
+from typing import Dict, Mapping, MutableSet, Optional, Union
 
 import github
-from github import Github, Repository, UnknownObjectException
+from github import Github, UnknownObjectException
+from github.GitRef import GitRef
+from github.Repository import Repository
 
 from komodo.prettier import write_to_string
 from komodo.yaml_file_types import (
     KomodoException,
-    ReleaseFile,
+    ReleaseMatrixFile,
     RepositoryFile,
     UpgradeProposalsFile,
 )
 
 
-def recursive_update(left, right):
-    if right is None:
+def recursive_update(
+    to_be_upgraded: Mapping[str, Union[str, Mapping[str, str]]],
+    upgrades: Optional[Mapping[str, Union[str, Mapping[str, str]]]],
+) -> None:
+    """Updates release in place with upgrades/additions of packages from upgrades.
+
+    Takes format:
+        package_name1: version_x
+
+        package_name2:
+                py38: version_y
+                py312: version_z
+
+        package_name3:
+                py38:
+                    rhel7: version_x
+                    rhel8: version_y
+    Args:
+        to_be_upgraded (Mapping[str, Union[str, Mapping[str, str]]]):
+
+        upgrades (Optional[Mapping[str, Union[str, Mapping[str, str]]]]):
+
+    """
+    if upgrades is None:
         return None
-    for key, value in right.items():
-        if isinstance(value, collections.abc.Mapping):
-            d_val = left.get(key)
-            if not d_val:
-                left[key] = value
-            else:
-                recursive_update(d_val, value)
+    for package_name, new_version_or_matrix in upgrades.items():
+        old_version_or_matrix = to_be_upgraded.get(package_name)
+        if isinstance(new_version_or_matrix, Mapping) and isinstance(
+            old_version_or_matrix, Mapping
+        ):
+            recursive_update(old_version_or_matrix, new_version_or_matrix)
         else:
-            left[key] = value
-    return left
+            to_be_upgraded[package_name] = new_version_or_matrix
+    return to_be_upgraded
 
 
-def _get_repo(token, fork, repo):
+def _get_repo(token: str, fork: str, repo: str) -> Repository:
     client = Github(token)
     try:
         return client.get_repo(f"{fork}/{repo}")
@@ -72,7 +95,7 @@ def main():
     )
 
 
-def verify_branch_does_not_exist(repo: Repository.Repository, branch_name: str) -> None:
+def verify_branch_does_not_exist(repo: Repository, branch_name: str) -> None:
     try:
         repo.get_branch(branch_name)
     except github.GithubException:
@@ -82,77 +105,62 @@ def verify_branch_does_not_exist(repo: Repository.Repository, branch_name: str) 
         raise ValueError(msg)
 
 
-def insert_proposals(repo, base, target, git_ref, jobname, joburl) -> None:
+def get_upgrade_key(target: str) -> str:
     year = target.split(".")[0]
     month = target.split(".")[1]
-    tmp_target = target + ".tmp"
+    return f"{year}-{month}"
 
-    # check that the branches do not already exist
-    verify_branch_does_not_exist(repo, target)
-    verify_branch_does_not_exist(repo, tmp_target)
 
-    # create contents of new release
-    proposals_yaml_string = load_yaml_from_repo("upgrade_proposals.yml", repo, git_ref)
-    proposal_file = UpgradeProposalsFile().from_yaml_string(proposals_yaml_string)
-    upgrade_key = f"{year}-{month}"
-    upgrade = proposal_file.content.get(upgrade_key)
-    proposal_file.validate_upgrade_key(upgrade_key)
+def validate_upgrades(
+    upgrade_section: Dict[str, str], repofile: RepositoryFile
+) -> None:
+    errors = set()
+    for package_name, package_version in upgrade_section.items():
+        recursive_validate_package_entries(
+            package_name, package_version, repofile, errors
+        )
+    if errors:
+        raise SystemExit("\n".join(errors))
 
-    base_file = f"releases/matrices/{base}.yml"
-    target_file = f"releases/matrices/{target}.yml"
-    repofile_yaml_string = load_yaml_from_repo("repository.yml", repo, git_ref)
-    repofile = RepositoryFile().from_yaml_string(repofile_yaml_string)
-    release_file_yaml_string = load_yaml_from_repo(base_file, repo, git_ref)
-    base_dict = ReleaseFile().from_yaml_string(release_file_yaml_string)
+
+def recursive_validate_package_entries(
+    package_name,
+    package_version_or_matrix: Union[str, Mapping],
+    repository_file: RepositoryFile,
+    errors: MutableSet,
+) -> None:
+    if isinstance(package_version_or_matrix, str):
+        try:
+            repository_file.validate_package_entry(
+                package_name, package_version_or_matrix
+            )
+        except KomodoException as komodo_exception:
+            errors.add(komodo_exception.error)
+    else:
+        for nested_version_or_matrix in package_version_or_matrix.values():
+            recursive_validate_package_entries(
+                package_name, nested_version_or_matrix, repository_file, errors
+            )
+
+
+def generate_contents_of_new_release_matrix(
+    base_release_matrix_content: Mapping,
+    repofile: RepositoryFile,
+    upgrade: Optional[Dict],
+) -> str:
     if upgrade:
-        errors = []
-        for package_name, package_version in upgrade.items():
-            try:
-                repofile.validate_package_entry(package_name, package_version)
-            except KomodoException as komodo_exception:
-                errors.append(komodo_exception.error)
-        if errors:
-            raise SystemExit("\n".join(errors))
-    recursive_update(base_dict.content, upgrade)
-    result = write_to_string(base_dict.content)
+        validate_upgrades(upgrade, repofile)
+    recursive_update(base_release_matrix_content, upgrade)
+    result = write_to_string(base_release_matrix_content)
+    return result
 
-    # create new release file
-    from_sha = repo.get_branch(git_ref).commit.sha
-    tmp_ref = repo.create_git_ref(ref="refs/heads/" + tmp_target, sha=from_sha)
-    repo.create_file(
-        target_file,
-        f"Add release {target}",
-        result,
-        branch=tmp_target,
-    )
 
-    # clean the proposal file
-    proposal_file.content[upgrade_key] = None
-    cleaned_upgrade = write_to_string(proposal_file.content, False)
-    upgrade_contents = repo.get_contents("upgrade_proposals.yml", ref=git_ref)
-    repo.update_file(
-        "upgrade_proposals.yml",
-        "Clean proposals",
-        cleaned_upgrade,
-        sha=upgrade_contents.sha,
-        branch=tmp_target,
-    )
-
-    # making PR
-    base_content = repo.get_contents(base_file, ref=git_ref)
-    diff = diff_file_and_string(
-        b64decode(base_content.content).decode(),
-        result,
-        base,
-        target,
-    )
-
-    pr_msg = f""":robot: Release {target}
+PR_MSG_TEMPLATE = """:robot: Release {target}
 ---
 ### Description
 - New Release: `{target}`
 - Based on: `{base}`
-- When: `{datetime.now()}`
+- When: `{timestamp}`
 
 ### Diff
 ```diff
@@ -166,6 +174,35 @@ _This pull request was generated by [{jobname}]({joburl})_.
 Source code for this script can be found [here](https://github.com/equinor/komodo).
 """
 
+
+def clean_proposals_file(
+    proposal_file_content: Mapping[str, Mapping],
+    upgrade_key: str,
+    repo: Repository,
+    git_ref: GitRef,
+    tmp_target,
+) -> None:
+    proposal_file_content[upgrade_key] = None
+    cleaned_upgrade = write_to_string(proposal_file_content, False)
+    upgrade_contents = repo.get_contents("upgrade_proposals.yml", ref=git_ref)
+    repo.update_file(
+        "upgrade_proposals.yml",
+        "Clean proposals",
+        cleaned_upgrade,
+        sha=upgrade_contents.sha,
+        branch=tmp_target,
+    )
+
+
+def create_pr_with_changes(
+    repo: Repository,
+    git_ref: GitRef,
+    target: str,
+    from_sha: str,
+    tmp_target: str,
+    tmp_ref: GitRef,
+    pr_msg: str,
+):
     repo.create_git_ref(ref="refs/heads/" + target, sha=from_sha)
     # making a temporary PR in order to squash the commits into one
     tmp_pr = repo.create_pull(
@@ -192,7 +229,88 @@ Source code for this script can be found [here](https://github.com/equinor/komod
     )
 
 
-def parse_args():
+def create_new_release_file(
+    repo: Repository, target: str, release_content: str, branch
+) -> str:
+    new_release_file = f"releases/matrices/{target}.yml"
+    repo.create_file(
+        new_release_file,
+        f"Add release {target}",
+        release_content,
+        branch=branch,
+    )
+    return new_release_file
+
+
+def insert_proposals(
+    repo: Repository, base: str, target: str, git_ref: str, jobname: str, joburl: str
+) -> None:
+    tmp_target = target + ".tmp"
+
+    verify_branch_does_not_exist(repo, target)
+    verify_branch_does_not_exist(repo, tmp_target)
+
+    upgrade_key = get_upgrade_key(target)
+
+    proposals_yaml_string = load_yaml_from_repo("upgrade_proposals.yml", repo, git_ref)
+    proposal_file = UpgradeProposalsFile().from_yaml_string(proposals_yaml_string)
+    proposal_file.validate_upgrade_key(upgrade_key)
+
+    base_file = f"releases/matrices/{base}.yml"
+    release_file_yaml_string = load_yaml_from_repo(base_file, repo, git_ref)
+    release_matrix_file = ReleaseMatrixFile.from_yaml_string(release_file_yaml_string)
+    upgrade: Dict[str, str] = proposal_file.content.get(upgrade_key)
+
+    repofile_yaml_string = load_yaml_from_repo("repository.yml", repo, git_ref)
+    repofile = RepositoryFile().from_yaml_string(repofile_yaml_string)
+
+    new_release_contents = generate_contents_of_new_release_matrix(
+        release_matrix_file.content, repofile, upgrade
+    )
+
+    from_sha = repo.get_branch(git_ref).commit.sha
+    tmp_ref = repo.create_git_ref(ref="refs/heads/" + tmp_target, sha=from_sha)
+    new_release_file = create_new_release_file(
+        repo, target, new_release_contents, tmp_target
+    )
+
+    clean_proposals_file(proposal_file.content, upgrade_key, repo, git_ref, tmp_target)
+
+    base_content = repo.get_contents(base_file, ref=git_ref)
+    diff = diff_file_and_string(
+        b64decode(base_content.content).decode(),
+        new_release_contents,
+        base,
+        target,
+    )
+    pr_msg = PR_MSG_TEMPLATE.format(
+        target=target,
+        base=base,
+        timestamp=datetime.now(),
+        base_file=base_file,
+        target_file=new_release_file,
+        diff=diff,
+        jobname=jobname,
+        joburl=joburl,
+    )
+    create_pr_with_changes(repo, git_ref, target, from_sha, tmp_target, tmp_ref, pr_msg)
+
+
+class InsertProposalsNamespace(argparse.Namespace):
+    """
+    Komodo Insert Proposals argument parser namespace
+    """
+
+    base: str
+    target: str
+    joburl: str
+    jobname: str
+    git_fork: str
+    git_repo: str
+    git_ref: str
+
+
+def parse_args() -> InsertProposalsNamespace:
     parser = argparse.ArgumentParser(
         description="Copy proposals into release and create PR.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -201,21 +319,21 @@ def parse_args():
         "base",
         type=str,
         help=(
-            "The name of the release to base on. (E.g. 2021.06.b0). "
+            "The name of the release matrix file to base on. (E.g. 2021.06.b0). "
             "A corresponding file must exist in releases/matrices"
         ),
     )
     parser.add_argument(
         "target",
         type=str,
-        help="The name of the new release file to create. (E.g. 2021.06.b0).",
+        help="The name of the new release matrix file to create. (E.g. 2021.06.b0).",
     )
     parser.add_argument("joburl", help="link to the job that triggered this")
     parser.add_argument("jobname", help="name of the job")
     parser.add_argument("--git-fork", help="git fork", default="equinor")
     parser.add_argument("--git-repo", help="git repo", default="komodo-releases")
     parser.add_argument("--git-ref", help="git ref", default="main")
-    return parser.parse_args()
+    return parser.parse_args(namespace=InsertProposalsNamespace())
 
 
 if __name__ == "__main__":
