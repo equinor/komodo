515dea68821f411221037ab608b2764765c625c4
Andreas Eknes Lie <andrli@equinor.com>
2024-11-12 10:27:39 +0100

    Fix existing test

--- tests/test_switch.py
+++ tests/test_switch.py
@@ -12,16 +12,34 @@ def test_write_activator_switches(tmpdir):
     switch.create_activator_switch(Data(), prefix, release)
 
     actual_bash_activator = prefix / f"{expected_release}/enable"
+    bash_source_script_path = "script_path=\"${BASH_SOURCE[0]}\""
+
     assert (
         actual_bash_activator.read_text(encoding="utf-8").strip()
         == f"""
 if [[ $(uname -r) == *el7* ]] ; then
-    export KOMODO_ROOT={prefix}
-    KOMODO_RELEASE_REAL={expected_release}
+    # Get the full path of the sourced script
+    { bash_source_script_path }
+    if [[ $script_path == *deprecated-rhel7* ]] ; then
+        export KOMODO_ROOT={ prefix }
+        KOMODO_RELEASE_REAL={ expected_release }
 
-    source $KOMODO_ROOT/$KOMODO_RELEASE_REAL-rhel7/enable
-    export PS1="(${{KOMODO_RELEASE_REAL}}) ${{_PRE_KOMODO_PS1}}"
-    export KOMODO_RELEASE=$KOMODO_RELEASE_REAL
+        source $KOMODO_ROOT/$KOMODO_RELEASE_REAL-rhel7/enable
+        export PS1="(${{KOMODO_RELEASE_REAL}}) ${{_PRE_KOMODO_PS1}}"
+        export KOMODO_RELEASE=$KOMODO_RELEASE_REAL
+    else
+        echo "Attention! Your machine is running on an environment 
+that is not supported. RHEL7 has been phased out.
+From October 2024, komodo versions only support RHEL8.
+Please migrate as soon as possible. 
+
+To use the latest stable RHEL7 build use 
+source /prog/res/komodo/deprecated-rhel7/enable
+
+If you have any questions or issues - 
+contact us on #ert-users on Slack or Equinor's Yammer.
+"
+    fi
 elif [[ $(uname -r) == *el8* ]] ; then
     export KOMODO_ROOT={prefix}
     KOMODO_RELEASE_REAL={expected_release}
